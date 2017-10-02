6192cd662b2b6f303a1e639559c70fa20169d7ef
Alumno <Alumno@W7Bsica>
2017-10-02 10:49:30 +0200

    Clases añadidas

---
+++ U0P05-Figuras/src/Figuras/Circunferencia.java
@@ -0,0 +1,10 @@
+package Figuras;
+
+public class Circunferencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ U0P05-Figuras/src/Figuras/Cuadrado.java
@@ -0,0 +1,10 @@
+package Figuras;
+
+public class Cuadrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
